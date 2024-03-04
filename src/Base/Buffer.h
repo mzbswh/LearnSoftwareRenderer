b41a896cd1ad52658e4b8551228f9787ff5ab161
lizonghuang <lizonghuang@wepie.com>
2024-03-04 18:38:43 +0800

    feat: MemoryUtils and partial Buffer

--- src/Base/Buffer.h
+++ src/Base/Buffer.h
@@ -46,6 +46,99 @@ namespace SoftGL
                 data_ = MemoryUtils::makeBuffer<T>(dataSize_, data);
             }
 
+            virtual void destroy()
+            {
+                width_ = 0;
+                height_ = 0;
+                innerWidth_ = 0;
+                innerHeight_ = 0;
+                dataSize_ = 0;
+                data_ = nullptr;
+            }
+
+            inline T* getRawDataPtr() const
+            {
+                return data_.get();
+            }
+
+            inline size_t getRawDataSize() const
+            {
+                return dataSize_;
+            }
+
+            inline size_t getRawDataByteSize() const
+            {
+                return dataSize_ * sizeof(T);
+            }
+
+            inline bool empty() const
+            {
+                return data_ == nullptr;
+            }
+
+            inline size_t getWidth() const
+            {
+                return width_;
+            }
+
+            inline size_t getHeight() const
+            {
+                return height_;
+            }
+
+            inline T* get(size_t x, size_t y)
+            {
+                T *ptr = data_.get();
+                if (ptr != nullptr && x < width_ && y < height_)
+                {
+                    return &ptr[convertIndex(x, y)];
+                }
+                return nullptr;
+            }
+
+            inline void set(size_t x, size_t y, const T &pixel)
+            {
+                T *ptr = data_.get();
+                if (ptr != nullptr && x < width_ && y < height_)
+                {
+                    ptr[convertIndex(x, y)] = pixel;
+                }
+            }
+
+            void copyRawDataTo(T *out, bool flip_y = false) const
+            {
+                T *ptr = data_.get();
+                if (ptr == nullptr) return;
+                if (!flip_y)
+                {
+                    memcpy(out, ptr, getRawDataByteSize());
+                }
+                else
+                {
+                    for (int i = 0; i < innerHeight_; i++)
+                    {
+                        memcpy(out + innerWidth_ * i, ptr + innerWidth_ * (innerHeight_ - 1 - i), innerWidth_ * sizeof(T));
+                    }
+                }
+            }
+
+            inline void clear() const
+            {
+                T *ptr = data_.get();
+                if (ptr == nullptr) return;
+                memset(ptr, 0, getRawDataByteSize());
+            }
+
+            inline void setAll(T val) const
+            {
+                T *ptr = date_.get();
+                if (ptr == nullptr) return;
+                for (int i = 0; i < dataSize_; i++)
+                {
+                    ptr[i] = val;
+                }
+            }
+
         protected:
             size_t width_ = 0;
             size_t height_ = 0;
--- src/Base/MemoryUtils.h
+++ src/Base/MemoryUtils.h
@@ -10,25 +10,69 @@ namespace SoftGL
 {
     class MemoryUtils
     {
-        public:
-            static void* alignedMalloc(size_t size, size_t alignment = SOFTGL_ALIGNMENT)
+    public:
+        static void *alignedMalloc(size_t size, size_t alignment = SOFTGL_ALIGNMENT)
+        {
+            if ((alignment & (alignment - 1)) != 0) // alignment需要是2的n次方
             {
-                if ((alignment & (alignment - 1)) != 0) // alignment需要是2的n次方
-                {
-                    LOGE("failed to malloc, invalid alignment: %d", alignment);
-                    return nullptr;
-                }
-                size_t extra = alignment + sizeof(void *);
-                void *data = malloc(size + extra);
-                if (!data)
-                {
-                    LOGE("Failed to malloc with size: %d", size);
-                    return nullptr;
-                }
-                size_t addr = (size_t) data + extra;
-                void *alignedPtr = (void *) (addr - (addr % alignment));
-                *((void **) alignedPtr - 1) = data;
-                return alignedPtr;
+                LOGE("failed to malloc, invalid alignment: %d", alignment);
+                return nullptr;
             }
+            size_t extra = alignment + sizeof(void *);
+            void *data = malloc(size + extra);
+            if (!data)
+            {
+                LOGE("Failed to malloc with size: %d", size);
+                return nullptr;
+            }
+            size_t addr = (size_t)data + extra;
+            void *alignedPtr = (void *)(addr - (addr % alignment));
+            *((void **)alignedPtr - 1) = data;
+            return alignedPtr;
+        }
+
+        static void alignedFree(void *ptr)
+        {
+            if (ptr)
+            {
+                free(((void **)ptr)[-1]);
+            }
+        }
+
+        static size_t alignedSize(size_t size)
+        {
+            if (size == 0)
+            {
+                return;
+            }
+            return SOFTGL_ALIGNMENT * std::ceil((float)size / (float)SOFTGL_ALIGNMENT);
+        }
+
+        template <typename T>
+        static std::shared_ptr<T> makeAlignedBuffer(size_t elemCnt)
+        {
+            if (elemCnt == 0)
+            {
+                return nullptr;
+            }
+            return std::shared_ptr<T>((T *)MemoryUtils::alignedMalloc(elemCnt * sizeof(T)), [](const T *ptr) { MemoryUtils::alignedFree((void *)ptr); })
+        }
+
+        template <typename T>
+        static std::shared_ptr<T> makeBuffer(size_t elemCnt, const uint8_t *data = nullptr)
+        {
+            if (elemCnt == 0)
+            {
+                return nullptr;
+            }
+            if (data != nullptr)
+            {
+                return std::shared_ptr<T>((T *)data, [](const T *ptr) {});
+            }
+            else
+            {
+                return std::shared_ptr<T>(new T[elemCnt], [](const T *ptr) { delete[] ptr; });
+            }
+        }
     };
 }
