c65fae7fc002bd34536997c35da7dab258112219
lizonghuang <lizonghuang@wepie.com>
2024-03-07 12:52:39 +0800

    feat: frustum.h

--- src/Base/Geometry.h
+++ src/Base/Geometry.h
@@ -63,4 +63,53 @@ namespace SoftGL
         glm::vec3 normal_;
         float d_ = 0;
     };
+
+    struct Frustum
+    {
+     public:
+        bool intersects(const BoundingBox &box) const;
+        // 与世界坐标的点相交结果(世界坐标)
+        bool intersects(const glm::vec3 &p0) const;
+        // 与线段相关结果(世界坐标)
+        bool intersects(const glm::vec3 &p0, const glm::vec3 &p1) const;
+        // 与三角形相交结果
+        bool intersects(const glm::vec3 &p0, const glm::vec3 &p1, const glm::vec3 &p2) const;
+
+     public:
+        // near far top bottom left right
+        Plane planes[6];
+        // nearTopLeft nearTopRight nearBottomLeft nearBottomRight farTopLeft farTopRight farBottomLeft farBottomRight
+        glm::vec3 corners[8];
+        BoundingBox bbox;
+    };
+
+    enum FrustumClipMask
+    {
+        POSITIVE_X = 1 << 0,
+        NEGATIVE_X = 1 << 1,
+        POSITIVE_Y = 1 << 2,
+        NEGATIVE_Y = 1 << 3,
+        POSITIVE_Z = 1 << 4,
+        NEGATIVE_Z = 1 << 5,
+    };
+
+    const int FrustumClipMaskArray[6] =
+    {
+        FrustumClipMask::POSITIVE_X,
+        FrustumClipMask::NEGATIVE_X,
+        FrustumClipMask::POSITIVE_Y,
+        FrustumClipMask::NEGATIVE_Y,
+        FrustumClipMask::POSITIVE_Z,
+        FrustumClipMask::NEGATIVE_Z,
+    };
+
+    const glm::vec4 FrustumClipPlane[6] =
+    {
+        {-1, 0, 0 ,1},
+        {1, 0, 0, 1},
+        {0, -1, 0, 1},
+        {0, 1, 0, 1},
+        {0, 0, -1, 1},
+        {0, 0, 1, 1}
+    };
 }
